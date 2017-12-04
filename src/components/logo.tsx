f554a617839bab8f569b495ffb302b2008cd7f4f
Kehao Chen <kehao.chen@happyhacking.ninja>
2017-12-04 16:14:26 +0800

    feat(ui): add logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Link from 'gatsby-link'
+import './logo.css';
+
+const Logo = () => (
+  <div className="logoPanel">
+    <div className="logoBorder">
+      <div className="logoFont">
+        Happy Hacking Ninja
+      </div>
+    </div>
+  </div>
+)
+
+export default Logo
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react'
 import Link from 'gatsby-link'
+import Logo from "../components/logo";
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Logo />
   </div>
 )
 
