6d21ccd20a9efa289fe5a10f6c288d2ee73f6f79
Kehao Chen <kehao.chen@happyhacking.ninja>
2017-12-09 11:42:49 +0800

    style(structure): move logo componect to a independent folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Link from 'gatsby-link'
-import Logo from "../components/logo";
+import Logo from "../components/Logo";
 import SocialLink from "../components/SocialLink"
 
 const IndexPage = () => (
